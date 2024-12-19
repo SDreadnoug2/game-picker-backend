74c49d310ae8a1a048894814cc92bf205c0e4be3
SDreadnoug2 <miltnr01@gmail.com>
2024-12-19 16:44:14 -0500

    openID implemented!

--- app.js
+++ app.js
@@ -3,26 +3,46 @@ const app = express();
 const cors = require("cors");
 const helmet = require("helmet");
 const router = express.Router();
+const session = require('express-session');
+const passport = require('passport');
 const mongoose = require("mongoose");
 const index = require("./routes/index");
 const { getAllGames } = require('./utils/fetchGames');
 const { setGamesList, setGamesListLength } = require('./utils/storeList');
-const SteamAuth = require("node-steam-openid");
-const steam = new SteamAuth({
-    realm: "https://pickagame.app",
-    returnUrl:"https://pickagame.app/libraries/userlibrary",
-    apiKey: "11CEE5DB82AEEFD5D66119105D65286C",
-})
-
+const fs = require('fs');
+const https = require('https');
 //json parser
 app.use(express.json());
-app.use(cors());
-app.use('/', index);
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(session({
+    secret: 'your-secret-key', // Replace with a secure secret key
+    resave: false,            // Avoid resaving session if unmodified
+    saveUninitialized: false, // Don't save uninitialized sessions
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user); // Save the whole user object in the session
+  });
+  
+  passport.deserializeUser((obj, done) => {
+    done(null, obj); // Retrieve the user from the session
+  });
+
+
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+  };
 
 mongoose.connect("mongodb://127.0.0.1:27017/SteamPicker")
 
 const dayMS = 1000 * 60 * 60 * 24;
-
 async function updateGamesList(){
     try{
         let games = await getAllGames();
@@ -39,7 +59,15 @@ updateGamesList();
 setInterval(() => updateGamesList, dayMS);
 
 const port = 3001;
+/*
 app.listen(3001, () => {
     console.log(`Server running on port: ${port}`);
 })
+*/
+
+app.use('/', index);
+
+https.createServer(options, app).listen(port, () => {
+    console.log(`Server running on https://localhost:${port}`);
+  });
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,38 @@
-const router = require("express").Router();
-const SteamAuth = require("node-steam-openid");
-const steam = new SteamAuth({
-    realm: "https://localhost:3001",
-    returnUrl:"https://localhost:3001/auth/steam/authenticate",
-    apiKey: "11CEE5DB82AEEFD5D66119105D65286C",
-});
+const express = require('express');
+const SteamStrategy = require("passport-steam");
+const router = require('express').Router();
+const passport = require('passport');
 
-router.get("/steam", async (req, res) => {
-    const redirectUrl = await steam.getRedirectUrl();
-    return res.redirect(redirectUrl);
+passport.use(new SteamStrategy({
+  returnURL: 'https://localhost:3001/auth/login',
+  realm: 'https://localhost:3001',
+  apiKey: '11CEE5DB82AEEFD5D66119105D65286C'
+},
+function(identifier, profile, done) {
+ const userProfile = {
+  steamID: profile.id,
+  name: profile.displayName,
+  avatar: profile.photos[0].value,
+ };
+ return done(null, userProfile);
+}));
+
+router.get('/login', passport.authenticate('steam', {failureRedirect: '/login/failure'}),
+  function(req, res) {
+    const user = req.user;
+
+    res.cookie('userData', JSON.stringify({
+      steamID: user.steamID,
+      name: user.name,
+      avatar: user.avatar,
+    }), {
+      httpOnly: false,
+      secure: true,
+      sameSite: 'Lax',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    const redirectUrl = `http://localhost:5173/libraries`;
+    res.redirect(redirectUrl);
   });
-  
-router.get("/steam/authenticate", async (req, res) => {
-try {
-    const user = await steam.authenticate(req);
-    console.log(user);
-    //This should redirect back, and then store the userID as a cookie, and
-    // then just have the cookie be removed when you click logout. 
-    // Explicitly state this, don't save any data whatsoever anywhere.
-    
-    return res.redirect("https://localhost:5173/libraries");
-} catch (error) {
-    console.error(error);
-}
-});
 
 module.exports = router;
